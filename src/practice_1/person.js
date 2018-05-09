8152aca29caccb85e23a13a3949de6d2ecd7a39f
liukxaa <644306737@qq.com>
2018-05-09 16:58:12 +0800

    完成 practice1

--- src/practice_1/person.js
+++ src/practice_1/person.js
@@ -1,1 +1,10 @@
-
+function Person(name, age) {
+    let person = {};
+    person.name = name;
+    person.age = age;
+    person.introduce = function () {
+        return `My name is ${name}. I am ${age} years old.`;
+    };
+    return person;
+}
+module.exports = Person;
