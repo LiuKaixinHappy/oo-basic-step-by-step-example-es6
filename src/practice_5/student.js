63083e57873b4099021fead365ec21cd197f69e0
liukxaa <644306737@qq.com>
2018-05-11 12:11:23 +0800

    完成 practice5

--- src/practice_5/student.js
+++ src/practice_5/student.js
@@ -1,1 +1,12 @@
+import Person from "../../src/practice_5/person.js";
+
+function Student(name, age, klass) {
+    Person.call(this, name, age);
+    this.klass = klass;
+    this.introduce = function () {
+        return `My name is ${name}. I am ${age} years old. I am a Student. I am at Class ${klass}.`;
+    };
+}
+
+module.exports = Student;
 
--- src/practice_5/worker.js
+++ src/practice_5/worker.js
@@ -1,1 +1,13 @@
+import Person from "../../src/practice_5/person.js";
+
+function Worker(name, age) {
+    Person.call(this, name, age);
+    this.introduce = function () {
+        return `My name is ${name}. I am ${age} years old. I am a Teacher. I have a job.`;
+    };
+}
+
+module.exports = Worker;
+
+
 
