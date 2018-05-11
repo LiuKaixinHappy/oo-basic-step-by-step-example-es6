66daba84ebdba852ebd9a46b5fdbca87242b5c31
liukxaa <644306737@qq.com>
2018-05-11 12:41:07 +0800

    完成 practice6

--- src/practice_6/teacher.js
+++ src/practice_6/teacher.js
@@ -1,1 +1,21 @@
+import Person from "../../src/practice_6/person.js";
+
+
+function Teacher(name, age, klass) {
+    Person.call(this, name, age);
+    this.klass = klass;
+    if (arguments.length < 3) {
+        this.introduce = function () {
+            return `My name is ${name}. I am ${age} years old. I am a Teacher. I teach No Class.`;
+        };
+    } else {
+        this.introduce = function () {
+            return `My name is ${name}. I am ${age} years old. I am a Teacher. I teach Class ${klass}.`;
+        };
+    }
+}
+module.exports = Teacher;
+
+
+
 
