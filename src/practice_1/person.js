179687246e4f45693ac0d64c85a2810a7113ff17
liukxaa <644306737@qq.com>
2018-05-11 11:39:14 +0800

    完成 practice2

--- src/practice_1/person.js
+++ src/practice_1/person.js
@@ -1,10 +1,8 @@
 function Person(name, age) {
-    let person = {};
-    person.name = name;
-    person.age = age;
-    person.introduce = function () {
+    this.name = name;
+    this.age = age;
+    this.introduce = function () {
         return `My name is ${name}. I am ${age} years old.`;
     };
-    return person;
 }
 module.exports = Person;
