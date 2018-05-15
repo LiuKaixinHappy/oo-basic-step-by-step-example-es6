8c1d2e44f14075100c97b5d0663081b3c3268f05
liukxaa <644306737@qq.com>
2018-05-15 19:16:41 +0800

    完成 practice8

--- src/practice_8/class.js
+++ src/practice_8/class.js
@@ -1,1 +1,12 @@
+function Class(number) {
+    this.number = number;
+    this.leader = undefined;
+}
+Class.prototype.getDisplayName = function () {
+    return `Class ${this.number}`;
+};
+Class.prototype.assignLeader = function(student) {
+    this.leader = student.id;
+};
+module.exports = Class;
 
--- src/practice_8/teacher.js
+++ src/practice_8/teacher.js
@@ -1,1 +1,18 @@
+import Person from "../../src/practice_8/person.js";
+function Teacher(id, name, age, klass) {
+    Person.call(this, id, name, age);
+    this.klass = klass;
+}
+Teacher.prototype = new Person();
+Teacher.prototype.introduce = function () {
+    let intro = [];
+    intro[0] = `${Person.prototype.introduce.call(this, arguments)} I am a Teacher.`;
+    if (this.klass === null || this.klass === undefined) {
+        intro[1] = 'I teach No Class.';
+    } else {
+        intro[1] = `I teach Class ${this.klass.number}.`;
+    }
+    return intro.join(' ');
+};
 
+module.exports = Teacher;
