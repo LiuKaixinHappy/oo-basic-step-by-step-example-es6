41dad9e53190daabc1abe4589bc8fd4f2706d74a
liukxaa <644306737@qq.com>
2018-05-15 21:13:46 +0800

    完成 practice10

--- src/practice_10/class.js
+++ src/practice_10/class.js
@@ -1,1 +1,22 @@
+function Class(number) {
+    this.number = number;
+    this.leader = undefined;
+}
+Class.prototype.getDisplayName = function () {
+    return `Class ${this.number}`;
+};
+Class.prototype.assignLeader = function (student) {
+    if (this.number === student.klass.number) {
+        student.klass.leader = student;
+    } else {
+        console.log('It is not one of us.');
+    }
+};
+Class.prototype.appendMember = function (student) {
+    student.klass = this;
+};
+Class.prototype.isIn = function (student) {
+    return this.number === student.klass.number;
+};
+module.exports = Class;
 
--- src/practice_10/teacher.js
+++ src/practice_10/teacher.js
@@ -1,1 +1,30 @@
+import Person from "../../src/practice_10/person.js";
+function Teacher(id, name, age, klasses) {
+    Person.call(this, id, name, age);
+    this.klasses = klasses;
+}
+Teacher.prototype = new Person();
+Teacher.prototype.introduce = function () {
+    let intro = [];
+    intro[0] = `${Person.prototype.introduce.call(this, arguments)} I am a Teacher.`;
+    if (this.klasses === null || this.klasses === undefined) {
+        intro[1] = 'I teach No Class.';
+    } else {
+        let klassesNum = [];
+        for (let klass of this.klasses) {
+            klassesNum.push(klass.number);
+        }
+        intro[1] = `I teach Class ${klassesNum.join(", ")}.`;
+    }
+    return intro.join(' ');
+};
+Teacher.prototype.isTeaching = function (student) {
+    for (let klass of this.klasses) {
+        if (klass.isIn(student)) {
+            return true;
+        }
+    }
+    return false;
+};
+module.exports = Teacher;
 
